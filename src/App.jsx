a5061ebba89e61eb25f8733cff3f14bfc6a0f111
ManuKashyap01 <kashyap.manu200201@gmail.com>
2023-10-04 17:03:32 +0530

    some minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,7 @@
 import Counter from './Counter'
-import { useState,useEffect } from 'react'
+import { useState} from 'react'
 import './App.css'
 
-function Counters({counterArray,setCounterArray}){
-  function deleteCounter(id){
-    const newarr=counterArray.filter(counter=>counter.id!=id)
-    setCounterArray([newarr])
-  }
-  console.log('here',counterArray)
-    return (
-      <div className='flex gap-4 flex-wrap'>
-        {counterArray.map((key)=>{
-          return <Counter id={key.id} deleteCounter={deleteCounter}/>
-        })}
-      </div>
-      // <div>Hello</div>
-    )
-}
 
 function App() {
   const [counterArray, setCounterArray] = useState([])
@@ -27,10 +12,18 @@ function App() {
     setCounterArray([...counterArray,newCounter])
   }
   // console.log(counterId,counterArray)
+  const deleteCounter=(id)=>{
+    const newarr=counterArray.filter(counter=>counter.id!=id)
+    setCounterArray(newarr)
+  }
   return (
     <div className='max-w-[750px] mx-auto my-4'>
       <button className='bg-black text-white rounded-sm px-8 py-4 mb-4' onClick={addCounter}>Add Counter</button>
-      <Counters counterArray={counterArray} setCounterArray={setCounterArray}/>
+      <div className='flex gap-4 flex-wrap'>
+        {counterArray.map((key)=>{
+          return <Counter key={key.id} id={key.id} deleteCounter={deleteCounter}/>
+        })}
+      </div>
     </div>
   )
 }
